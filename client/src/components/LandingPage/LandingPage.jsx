6f26f9148a6e82db7b201ccd1edf26e9225875c6
viru <ezequielprovision@hotmail.com>
2021-06-09 09:15:45 -0300

    LandingPage style updated, parallax swiper added

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 
+import Parallax from './Parallax.jsx'
 import './LandingPage.css'
 
 const LandingPage = () => {
     return (
-        <div className='landingPageTitle displayFlexRow'> 
-            <h1 className='subtitleFont'>
-                The Prescriptions Registry
-            </h1>
-            <div id='landingPageSubtitle'>
-                <h3 className='h3Font'>
-                    The website that takes cares about your eyes
-                </h3>
+        <div >
+            <div className='landingPageTitle displayFlexRow'> 
+                <h1 className='titleFont'>
+                    The Prescriptions Registry
+                </h1>
+                <div id='landingPageSubtitle'>
+                    <h3 className='h3Font'>
+                        The website for your eyes care
+                    </h3>
+                    
+                    {/* https://www.genomicseducation.hee.nhs.uk/wp-content/uploads/2019/11/Eye-DNA-1350x450px-1345x450.jpg */}
+                </div>
             </div>
+            <Parallax 
+                url={'https://www.genomicseducation.hee.nhs.uk/wp-content/uploads/2019/11/Eye-DNA-1350x450px-1345x450.jpg'}
+            />
             
         </div>
     )
