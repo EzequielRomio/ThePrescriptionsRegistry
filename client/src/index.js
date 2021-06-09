913e57f9c127befedb28906b337701c8a5faf8c6
viru <ezequielprovision@hotmail.com>
2021-06-08 23:02:19 -0300

    client template done

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,28 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {BrowserRouter} from 'react-router-dom';
+import { Provider } from "react-redux";
+import store from './store/Store.js'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './index.css';
+
+dotenv.config()
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
