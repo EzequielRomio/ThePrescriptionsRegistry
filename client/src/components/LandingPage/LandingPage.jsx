c636b663ca054bb9ca407c6302a1c658a133872a
Ezequiel Romio <romio.ezequiel@gmail.com>
2021-06-12 09:57:51 -0300

    landing page style updated

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -8,17 +8,7 @@ import lenses_manuf from '../../images/contact_lenses.mp4';
 const LandingPage = () => {
     return (
         <div id='landingPageContainer'>
-            {/* <div className='landingPageTitle displayFlexRow'> 
-                <h1 className='titleFont'>
-                    The Prescriptions Registry
-                </h1>
-                <div id='landingPageSubtitle'>
-                    <h3 className='h3Font'>
-                        The way to take <br></br> care of your eyes
-                    </h3>
-                </div>
-            </div> */}
-
+            
             <Parallax 
                 url={'https://www.genomicseducation.hee.nhs.uk/wp-content/uploads/2019/11/Eye-DNA-1350x450px-1345x450.jpg'}
             />
@@ -60,12 +50,18 @@ const LandingPage = () => {
                         Sometimes working with a lot of patients could be a problem to have a Registry
                         of all their names, eyes disorders, last date, etc... These days are over.
                     </p>
+
+                    <div className='marginTop'></div>
+                    
                     <h2 className='subtitleFont'>Lens content</h2>
                     <p>
                         A corrective lens is a lens that is typically worn in front of the eye to improve daily vision. 
                         The most common use is to treat refractive errors: myopia, hypermetropia, astigmatism, and presbyopia. 
                         Glasses or "spectacles" are worn on the face a short distance in front of the eye. 
                     </p>
+
+                    <div className='marginTop'></div>
+
                     <h2 className='subtitleFont'>Technical Support</h2>
                     <p>
                         At <bold>ThePresctiptionsRegistry</bold>, you’ll find a dedicated team of opticians 
@@ -73,6 +69,7 @@ const LandingPage = () => {
                         We are committed to providing the highest quality care while utilizing the latest advancements 
                         in diagnostic and medical treatments.
                     </p>
+
                 </div>
             </div>
         </div>
