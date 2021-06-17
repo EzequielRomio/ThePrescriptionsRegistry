c2e5cf9ca0dc481849e84864701f27f0bb3ffa92
EzequielRomio <romio.ezequiel@gmail.com>
2021-06-17 19:09:17 -0300

    patients table and model created

---
+++ api/sqls/03_patients_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE patients (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    age INTEGER,
+    provider TEXT NOT NULL,
+    email TEXT NOT NULL   
+);
