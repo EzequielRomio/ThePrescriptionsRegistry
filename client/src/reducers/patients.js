a1ae0507f20bebd3f8941316651bf4a09ee66eeb
EzequielRomio <romio.ezequiel@gmail.com>
2021-06-20 11:30:29 -0300

    patients reducer created

---
+++ client/src/reducers/patients.js
@@ -0,0 +1,17 @@
+const initialState = {
+    patients: []
+}
+
+const patientsReducer = (state=initialState, action) => {
+    switch (action.type) {
+        case 'POST_PATIENT': 
+            return {
+                ...state, patients: [...state.patients, action.payload]
+            }
+        
+        default:
+            return state;
+    }
+}
+
+export default patientsReducer;
